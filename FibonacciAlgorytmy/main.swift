6c19f7fb255c4cdde2c671532b453e17d94111ce
Michał Czerniakowski <mpczerniakowski@gmail.com>
2016-11-04 20:45:57 +0100

    Initial Commit

---
+++ FibonacciAlgorytmy/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FibonacciAlgorytmy
+//
+//  Created by Michał Czerniakowski on 04.11.2016.
+//  Copyright © 2016 Michał Czerniakowski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
